c501e103eb851c42d1994ed983e9f1990412cd1d
Nayan Jyoti Bhuyan <n.bhuyan2014@gmail.com>
2021-02-13 12:53:42 +0530

    add a controller and a router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,4 @@
+module.exports.home = function(req,res)
+{
+    res.end('<h1>Express is up for codeial</h1>');
+}
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.use('/',require('./routes/index'));
+
 app.listen(port,function(err)
 {
     if(err)
