fb32f748baeb2150a254b5d6afadde45c772b29f
Nayan Jyoti Bhuyan <n.bhuyan2014@gmail.com>
2021-03-25 15:53:50 +0530

    prepopulating the user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,36 @@
+const Post = require('../models/post');
 module.exports.home = function(req,res)
 {
     console.log(req.cookies);
     res.cookie('user_id',25);
-   return res.render('home',
-   {
-       title:"Home"
-   });
+    // Post.find({},function(err,posts)
+    // {
+    //     if(err)
+    //     {
+    //         console.log('Error finding the posts');
+    //         return;
+    //     }
+    //     return res.render('home',
+    //     {
+    //         title:"Home",
+    //         posts:posts
+    //     });
+    // });
+
+    
+    // Previous method can  aloso be used
+    // But As we need the name so we are populating the whole user object
+    Post.find({}).populate('user').exec(function(err,posts)
+    {
+        if(err)
+        {
+            console.log('Error finding the posts');
+            return;
+        }
+        return res.render('home',
+        {
+            title:"Home",
+            posts:posts
+        });
+    })
 }
