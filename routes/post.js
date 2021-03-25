d9904f88f05b59b760149b9828d69a85b49a8d5c
Nayan Jyoti Bhuyan <n.bhuyan2014@gmail.com>
2021-03-25 17:06:08 +0530

    Check Authentication on Creating Post

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
 const PostController = require('../controllers/post_controller');
-router.post('/create',PostController.create);
+// Passport.checkAuthentication is used so that no one can post without signing in
+// If an outsider sign-in and checks the form action url and other attrributes than
+// he can sign-out and edit the html home page using inspect and create a form with
+// the attributes and action then he will be prevented from posting using this authentication
+router.post('/create',passport.checkAuthentication,PostController.create);
 module.exports = router;
